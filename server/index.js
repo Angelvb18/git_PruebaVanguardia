7c2cb8205ef966793422bf4fecf4846a8d5bdc07
Angelvb18 <angeljosevb12@gmail.com>
2023-12-06 00:38:38 -0600

    Cambios Necesarios para comunucacion

--- server/index.js
+++ server/index.js
@@ -1,24 +1,26 @@
-const bodyParser = require('body-parser')
-const express = require('express')
+const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');  // Agrega esta línea
 
-const connectDb = require('./db.js')
-const studentRoutes = require('./controllers/student.controller.js')
-const ingredientesRoutes = require('./controllers/ingredientes.controller.js')
-const pizzaRoutes = require('./controllers/pizza.controllers.js')
-const menuRoutes = require('./controllers/menu.controller.js')
+const connectDb = require('./db.js');
+const studentRoutes = require('./controllers/student.controller.js');
+const ingredientesRoutes = require('./controllers/ingredientes.controller.js');
+const pizzaRoutes = require('./controllers/pizza.controllers.js');
+const menuRoutes = require('./controllers/menu.controller.js');
 
-const app = express()
+const app = express();
 
+app.use(cors());  // Agrega esta línea para habilitar CORS
 app.use(bodyParser.json());
-app.use('/api/student',studentRoutes)
-app.use('/api/ingredientes',ingredientesRoutes)
-app.use('/api/pizza',pizzaRoutes)
-app.use('/api/menu',menuRoutes)
+app.use('/api/student', studentRoutes);
+app.use('/api/ingredientes', ingredientesRoutes);
+app.use('/api/pizza', pizzaRoutes);
+app.use('/api/menu', menuRoutes);
 
-app.listen(3000,()=>console.log('server started at 3000'))
+app.listen(4000, () => console.log('server started at 4000'));
 
 connectDb()
-    .then(()=>{
-        console.log("db connection succededs")
-    })
-    .catch(err => console.log(err))
+  .then(() => {
+    console.log('db connection succeeded');
+  })
+  .catch((err) => console.log(err));
