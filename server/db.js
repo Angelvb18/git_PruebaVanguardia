dc1cd9ced1b69a7c10e45d3e772686bcf0a1da6c
Angelvb18 <angeljosevb12@gmail.com>
2023-12-05 19:24:24 -0600

    env

--- server/db.js
+++ server/db.js
@@ -1,8 +1,9 @@
 const { ServerApiVersion } = require('mongodb')
 const mongoose = require('mongoose')
+require('dotenv').config();
 //mongodb+srv://angeljosevb12:AXf38TQRKEEMHyHb@cluster0.zrhwame.mongodb.net/
-const dbUrl = 'mongodb+srv://angeljosevb12:AXf38TQRKEEMHyHb@cluster0.zrhwame.mongodb.net/vanguard_cd?retryWrites=true&w=majority'
-
+//const dbUrl = 'mongodb+srv://angeljosevb12:AXf38TQRKEEMHyHb@cluster0.zrhwame.mongodb.net/vanguard_cd?retryWrites=true&w=majority'
+const dbUrl = process.env.DBURL;
 module.exports = () => {
     return mongoose.connect(dbUrl,{
        useNewUrlParser: true , 
